92a7eaafe1503621ee9dcbe8c081c66fab61a894
Marcos <exegeses@gmail.com>
2021-08-21 16:27:25 -0300

    Panel de administración de marcas

--- catalogo/routes/web.php
+++ catalogo/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+##################################
+#### CRUD de marcas
+use App\Http\Controllers\MarcaController;
+Route::get('/adminMarcas', [ MarcaController::class, 'index' ]);
+Route::get('/agregarMarca', [ MarcaController::class, 'create' ]);
+
+
+####################################
+#### CRUD de categorias
