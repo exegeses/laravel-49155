cf79bb0bd9545aced7e95c2853bac0c5ef5de616
Marcos <exegeses@gmail.com>
2021-08-28 16:49:44 -0300

    vista de confirmacion de baja de una marca (sin terminar)

--- catalogo/app/Http/Controllers/MarcaController.php
+++ catalogo/app/Http/Controllers/MarcaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Marca;
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
@@ -110,6 +111,28 @@ class MarcaController extends Controller
             ->with([ 'mensaje'=>'Marca: '.$mkNombre.' modificada correctamente.' ]);
     }
 
+    private function productoPorMarca($idMarca)
+    {
+        //$check = Producto::where('idMarca', $idMarca)->first();
+        //$check = Producto::firstWhere('idMarca', $idMarca);
+        $check = Producto::where('idMarca', $idMarca)->count();
+        return $check;
+    }
+
+    public function confirmarBaja($id)
+    {
+        //obtenemos datos de Marca por su id
+        $Marca = Marca::find($id);
+
+        //si NO HAY productos de esa marca
+        if ( $this->productoPorMarca($id) == 0 ){
+            //retornamos vista de confirmación
+            return view('eliminarMarca', [ 'Marca'=>$Marca ]);
+        }
+        //redirección con mensaje que no se puede eliminar
+        return redirect('/adminMarcas')
+                ->with(['mensaje'=>'No se puede eliminar la marca: '.$Marca->mkNombre.' ya que tiene productos relacionados.']);
+    }
     /**
      * Remove the specified resource from storage.
      *
