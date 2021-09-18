97ee9637e01a8d6deaaff4faaa38c14440734cc9
Marcos <exegeses@gmail.com>
2021-09-18 17:15:25 -0300

    Práctica Migraciones, Seeders

---
+++ login/database/migrations/2021_09_18_181747_create_marcas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarcasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marcas', function (Blueprint $table) {
+            $table->tinyIncrements('idMarca');//primaryKey tinyInteger
+            $table->string('mkNombre', 30);
+            //$table->timestamps();// updated_at && created_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marcas');
+    }
+}
